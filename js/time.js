3b21c7608e5eab4f3e9b3bb51399403433f3a639
Haneul Jin <77265562+knsan189@users.noreply.github.com>
2021-01-29 11:08:38 +0900

    Add files via upload

--- js/time.js
+++ js/time.js
@@ -1,30 +1,43 @@
+$(function () {
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        //console.log(scroll);
+        if (scroll >= 20) {
+            //console.log('a');
+            $("#header").addClass("fixed");
+        } else {
+            //console.log('a');
+            $("#header").removeClass("fixed");
+        }
+    });
+});
 
 $(function () {
     $('.menu_btn').click(function () {
-       $('#nav').addClass('menu_on');
+        $('#nav').addClass('menu_on');
     })
     $('.menu_close').click(function () {
-       $('#nav').removeClass('menu_on');
+        $('#nav').removeClass('menu_on');
     });
     $('.depth1>li').click(function () {
-       $('#nav').removeClass('menu_on');
+        $('#nav').removeClass('menu_on');
     });
 });
 
 
-(function($){
-    $.fn.extend({ 
+(function ($) {
+    $.fn.extend({
 
-        addTemporaryClass: function(className, duration) {
+        addTemporaryClass: function (className, duration) {
             var elements = this;
-            setTimeout(function() {
+            setTimeout(function () {
                 elements.removeClass(className);
             }, duration);
 
-            return this.each(function() {
+            return this.each(function () {
                 $(this).addClass(className);
             });
         }
     });
-    
+
 })(jQuery);
